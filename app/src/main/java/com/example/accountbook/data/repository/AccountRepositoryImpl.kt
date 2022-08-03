ae0a611e386a0c0d803810971112494d9f211e94
DAEHYEONJO <jdhqpt12@gmail.com>
2022-08-04 01:22:08 +0900

    [Chore] Calendar Item List로직 Usecase로 분리 및 PieChart UI 개발 * repository단은 data source로부터 data를 전달받는 역할을 하고 * usecase 단에서 presentation으로 전달할 데이터를 가공하여 * ViewModel로 전달해야 할 것 으로 판단하여 수정

--- app/src/main/java/com/example/accountbook/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/com/example/accountbook/data/repository/AccountRepositoryImpl.kt
@@ -12,6 +12,7 @@ import com.example.accountbook.di.IoDispatcher
 import com.example.accountbook.domain.model.CalendarItem
 import com.example.accountbook.domain.model.HistoriesListItem
 import com.example.accountbook.domain.model.HistoriesTotalData
+import com.example.accountbook.domain.model.StatisticsItem
 import com.example.accountbook.domain.repository.AccountRepository
 import com.example.accountbook.utils.dateToYearMonthDay
 import kotlinx.coroutines.CoroutineDispatcher
@@ -21,6 +22,7 @@ import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 @Singleton
 class AccountRepositoryImpl @Inject constructor(
@@ -86,73 +88,26 @@ class AccountRepositoryImpl @Inject constructor(
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override suspend fun getCalendarItems(
+    override suspend fun getCalendarHashMap(
         isExpense: Int,
         start: Long,
-        end: Long,
-        year: Int,
-        month: Int,
-        day: Int
-    ): List<CalendarItem>{
-        Log.e(TAG, "getCalendarItems: $year $month $day", )
-        val tempCalendarItem = ArrayList<CalendarItem>()
-        val monthCalendar = Calendar.getInstance()
-        monthCalendar.set(year, month-1, day)
-        monthCalendar[Calendar.DAY_OF_MONTH] = 1
-        val firstDayOfMonth = monthCalendar[Calendar.DAY_OF_WEEK] - 1
-        monthCalendar.add(Calendar.DAY_OF_MONTH, -firstDayOfMonth)
-
-        var secondWeekCount = 0
-
-        // 실제 오늘의 날짜를 확인하기 위함
-        val(_, todayMonth, todayDay) = dateToYearMonthDay(Calendar.getInstance().time)
-
-        // 현재 확인하려는 달의 date
-        val nowLocalDate = LocalDate.of(year, month, todayDay)
-        val nowMonth = nowLocalDate.month.value
-
-        while (tempCalendarItem.size < 42) {
-            val weekOfMonth = monthCalendar[Calendar.WEEK_OF_MONTH]
-
-            if (weekOfMonth == 2) secondWeekCount++
-            if (secondWeekCount == 8) break
-
-            val curDay = monthCalendar[Calendar.DAY_OF_MONTH]
-            val curMonth = monthCalendar[Calendar.MONTH]+1
-            val dayOfWeek = monthCalendar[Calendar.DAY_OF_WEEK]
-
-            tempCalendarItem.add(
-                CalendarItem(
-                    date = monthCalendar.time,
-                    year = year,
-                    month = curMonth,
-                    day = curDay,
-                    isSaturday = dayOfWeek==7,
-                    isThisMonth = nowMonth == curMonth,
-                    isToday = todayMonth==curMonth && todayDay==curDay && (nowMonth==curMonth)
-                )
+        end: Long
+    ): HashMap<String, CalendarItem> {
+        return withContext(dispatcher){
+            dbHelper.getHistoriesTotalDataGroupByDay(
+                isExpense, start, end
             )
-            monthCalendar.add(Calendar.DAY_OF_MONTH, 1)
         }
-        return withContext(dispatcher) {
-            val dayHashMap = dbHelper.getHistoriesTotalDataGroupByDay(isExpense, start, end)
-            dayHashMap.forEach {
-                Log.e(TAG, "getCalendarItems: $it", )
-            }
-            tempCalendarItem.map { calendarItem ->
-                with(calendarItem){
-                    val dayString = "${this.year} ${this.month} ${this.day}"
-                    if (dayHashMap.containsKey(dayString)){
-                        Log.e(TAG, "getCalendarItems: yes", )
-                        val curCalendarItem = dayHashMap[dayString]
-                        incomePrice = curCalendarItem!!.incomePrice
-                        expensePrice = curCalendarItem.expensePrice
-                        totalPrice = curCalendarItem.totalPrice
-                    }
-                }
-                calendarItem
-            }
+    }
+
+    override suspend fun getStatisticsItems(
+        isExpense: Int,
+        start: Long,
+        end: Long
+    ): List<StatisticsItem> {
+        return withContext(dispatcher){
+
+            emptyList()
         }
     }
 
